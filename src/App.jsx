93332edb054a891cee0de8c155f6d8d875bb09f0
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-02 22:58:59 +0530

    Creating parallel section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import './app.scss';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Parallax from './components/parallax/Parallax';
 
 const App = () => {
   return (
     <div>
-      <section id='Homepage'>
+      <section id="Homepage">
         <Navbar />
         <Hero />
       </section>
-      <section id='Services'>Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Servicess</section>
-      <section id='Portfolio'>Parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section id='Contact'>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

ceb42195d695402e7667240afb50576e56b12438
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-03 09:07:35 +0530

    creates services page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './app.scss';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
+import Services from './services/Services';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
       <section id="Services">
         <Parallax type="services" />
       </section>
-      <section>Servicess</section>
+      <section><Services /></section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
